293c55b25be074e0a49441c3601374cfc5df7040
Patricio Mariano <34042558+patomdp@users.noreply.github.com>
2022-08-12 12:43:11 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+// Link al repositorio de Open Bootcamp -> https://github.com/Open-Bootcamp/JavaScript-Basico
+// Esta es el mensaje de bienvenida
+console.log('Esta es la puerta de entrada al proyecto 🚪');
+
+
+// Esta es la parte intermedia del proyecto
+// En esta parte realizamos los chequeos necesarios
+// Por ultimo vamos a la ultima linea de saludo.
+
+
+// Este es el mensaje para despedirnos
+console.log('Adios');
+
